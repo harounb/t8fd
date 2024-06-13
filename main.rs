dc689034b804d6dfff3d73d9e305ae62a8774bf0
= <7194672+harounb@users.noreply.github.com>
2024-06-13 22:57:41 +0100

    Add pull command for new data; Add iterating over pulled data

--- main.rs
+++ main.rs
@@ -1,8 +1,10 @@
 use std::{fs, path::Path, io};
+use std::process::Command;
 use tera::{Context, Tera};
 use serde_json;
 
 fn build_templates() {
+    let dirs = fs::read_dir("data/TekkenFramedataAPI/src/__data/tekken8").expect("failed to read dir");
     // Use globbing
     let tera = match Tera::new("templates/**/*.html") {
         Ok(t) => t,
@@ -11,15 +13,35 @@ fn build_templates() {
             ::std::process::exit(1);
         }
     };
-    let mut context = Context::new();
-    context.insert("title",&"Tekken 8 Frame Data - Hwoarang");
-    let file = fs::read_to_string("data/hwoarang.json").expect("Unable to read file");
-    let data: serde_json::Value = serde_json::from_str(&file).expect("JSON was not well-formatted");
-    context.insert("data", &data);
-    context.insert("displayCharacter", "Hwoarang");
-    let rendered = tera.render("page.html", &context).expect("rendering failed");
-    let _ = fs::write("build/page.html", rendered);
+
+    for file in dirs {
+        let mut context = Context::new();
+        let mut file_path:String = "data/TekkenFramedataAPI/src/__data/tekken8/".to_owned();
+        let file_name = file.unwrap().file_name().into_string().unwrap();
+        file_path = file_path + &file_name;
+        context.insert("title",&("Tekken 8 Frame Data - ".to_owned() + &file_name));
+        let file_json = fs::read_to_string(&file_path).expect("Unable to read file");
+        let data: serde_json::Value = serde_json::from_str(&file_json).expect("JSON was not well-formatted");
+        context.insert("data", &data);
+        context.insert("displayCharacter", &file_name);
+        let output_file_path = "build/".to_owned() + &file_name + &".html";
+        let rendered = tera.render("page.html", &context).expect("rendering failed");
+        let _ = fs::write(&output_file_path, rendered);
+    }
+   
     let _ = copy_dir_all("static", "build");
+}
+
+fn pull_latest_frame_data() {
+let _ = fs::remove_dir_all("data/");
+let _ = fs::create_dir("data/");
+Command::new("git")
+.arg("clone")
+.arg("--depth=1")
+.arg("https://github.com/FrostemanNeogard/TekkenFramedataAPI.git")
+.current_dir("data/")
+.spawn()
+.expect("git clone failed");
 }
 
 // https://stackoverflow.com/a/65192210
@@ -40,9 +62,10 @@ fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> 
 
 fn main() {
     let cmd = std::env::args().nth(1).expect("no command given");
-// let data = octocrab::instance().pulls("FrostemanNeogard", "TekkenFramedataAPI").get(5).await?;
     if cmd == "build" {
         build_templates();
+    } else if cmd =="pull" {
+        pull_latest_frame_data();
     } else {
         print!("invalid command")
     }
